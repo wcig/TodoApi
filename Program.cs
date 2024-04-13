ac29011dacabcecdeb5db4085cdfde8115289cf5
wcig <job.yangbo@outlook.com>
2024-04-13 21:38:17 +0800

    add TodoItem controller

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
